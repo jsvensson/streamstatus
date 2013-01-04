033c164ee261866cfa074166371f3b8575071220
Johan Svensson <johan@atomicplayboy.net>
2013-01-04 11:39:54 +0100

    First test

--- app.rb
+++ app.rb
@@ -1,9 +1,35 @@
 require 'rubygems'
 require 'bundler'
-require 'sinatra'
+Bundler.require :default, (ENV['RACK_ENV'] || "development").to_sym
 
 Dir["./lib/*.rb"].each { |f| require f }
 
+set :haml, format: :html5, attr_wrapper: %{"}
+set :title, "Stream status"
+
+# Streams to monitor
+# [id, name, :service]
+set :streams, streams = [
+		{
+			id: 131174,
+			name: "Echo",
+			service: :own3d
+		},
+		{
+			id: "DanDinh",
+			name: "Dan Dinh",
+			service: :twitch
+		}
+	]
+
+settings.streams.each do |s|
+	stream = Stream.new(s[:id], s[:service])
+	s[:is_live] = stream.is_live?
+end
+
 get '/' do
-	"Hello, world"
+	@title = settings.title
+	@streams = settings.streams
+
+	haml :index
 end
