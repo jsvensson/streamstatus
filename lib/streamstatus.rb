44c1b69ba498789cd12bbeafa0f20e50ff41e1cf
Johan Svensson <johan@atomicplayboy.net>
2013-01-05 19:19:20 +0100

    Big overhaul

--- lib/streamstatus.rb
+++ lib/streamstatus.rb
@@ -1,61 +1,74 @@
 require 'rubygems'
 require 'bundler/setup'
+#Bundler.require :default, (ENV['RACK_ENV'] || "development").to_sym
 require 'httparty'
 
 class Stream
+
+	attr_reader :name, :viewers
 
 	StreamUri = {
 		own3d: "http://api.own3d.tv/rest/live/status.json?liveid=",
 		twitch: "http://api.justin.tv/api/stream/list.json?channel="
 	}
 
-	def initialize(stream_id, service = :own3d)
+	def initialize(stream_id, service = :own3d, options = {})
+		@options = options
 		@service = service
 		@stream_id = stream_id
-		@stream_uri = StreamUri[@service] + @stream_id.to_s
+
+		if @options[:file]
+			@stream_uri = @options[:file]
+		else
+			@stream_uri = StreamUri[@service] + @stream_id.to_s
+		end
+
 		get_status
 	end
 
 	def is_live?
-		case @service
-		when :own3d
-			@response['live_is_live'].to_i == 1
-		when :twitch
-			@response["format"] == "live"
-		end
-	end
-
-	def name
-		case @service
-		when :own3d
-			@response["live_name"]
-		when :twitch
-			@response["name"]
-		end
-
-	end
-
-	def viewers
-		case @service
-		when :own3d
-			@response["live_viewers"].to_i
-		when :twitch
-			@response["channel_count"]
-		end
+		@is_live
 	end
 
 	private
 
 	def get_status
-		puts "## Stream URI: #{@stream_uri}"
-		puts "## Getting JSON for #{@service.capitalize} stream #{@stream_id}"
-		@response = HTTParty.get(@stream_uri)
+#		puts "## Stream URI: #{@stream_uri}"
+#		puts "## Getting JSON for #{@service.capitalize} stream #{@stream_id}"
+		if @options[:file]
+			f = File.read(@stream_uri)
+			response = MultiJson.load(f)
+		else
+			response = HTTParty.get(@stream_uri)
+		end
 
-		# Cleanup for Twitch
 		case @service
 		when :twitch
-			@response = @response[0]
+			@response = build_twitch(response)
+		when :own3d
+			@response = build_own3d(response)
 		end
+	end
+
+	def build_twitch(data)
+		# Twitch puts the stream data in an array, move it up
+		data = data[0]
+
+		if data
+			@is_live = true
+			@name    = data['title']
+			@viewers = data['channel_count']
+		else
+			@is_live = false
+			@name    = ''
+			@viewers = 0
+		end
+	end
+
+	def build_own3d(data)
+		@is_live = data['live_is_live'].to_i == 1
+		@name    = data['live_name']
+		@viewers = data['live_viewers'].to_i
 	end
 
 end
