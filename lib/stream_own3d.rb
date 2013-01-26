f0566a88abbfa8e08e235cf1e88d12c3ccb52227
Johan Svensson <johan@atomicplayboy.net>
2013-01-26 13:26:38 +0100

    @stream_uri passed from initialize()

--- lib/stream_own3d.rb
+++ lib/stream_own3d.rb
@@ -8,7 +8,6 @@ class Stream
 			@is_live    = data['live_is_live'].to_i == 1
 			@name       = data['live_name']
 			@viewers    = data['live_viewers'].to_i
-			@stream_uri = "http://www.own3d.tv/#{data['channel_name']}/live/#{data['live_id']}"
 			@json_uri   = "http://api.own3d.tv/rest/live/status.json?liveid=#{@stream_id}"
 		end
 
