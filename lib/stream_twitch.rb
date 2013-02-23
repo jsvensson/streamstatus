6d14fedcaac66abd63e55d215b3f4ad003b974a0
Johan Svensson <johan@atomicplayboy.net>
2013-02-23 16:38:00 +0100

    Alignment OCD

--- lib/stream_twitch.rb
+++ lib/stream_twitch.rb
@@ -7,16 +7,16 @@ class Stream
 		def build(data)
 			# Twitch puts the stream data in an array, move it up
 			data = data[0]
-			@json_uri   = "http://api.justin.tv/api/stream/list.json?channel=#{@stream_id}"
+			@json_uri = "http://api.justin.tv/api/stream/list.json?channel=#{@stream_id}"
 			if data
-				@is_live    = true
-				@viewers    = data['channel_count']
+        @is_live  = true
         @title    = data['title']
         @username = data['name']
+        @viewers  = data['channel_count']
 			else
-				@is_live    = false
-				@viewers    = 0
+        @is_live = false
         @title   = "Offline Twitch/Justin stream"
+        @viewers = 0
 			end
 		end
 
