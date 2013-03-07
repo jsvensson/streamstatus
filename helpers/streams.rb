3f5edf731510a15570624f81804f47e741e922b0
Johan Svensson <johan@atomicplayboy.net>
2013-03-07 13:32:54 +0100

    Service-aware helper

--- helpers/streams.rb
+++ helpers/streams.rb
@@ -1,7 +1,13 @@
 helpers do
 
   def update_stream(stream_uri, cache)
-    str = Stream::Twitch.new(stream_uri, {update: false})
+    info = Stream::Service.normalize(stream_uri)
+    case info[:service]
+    when :twitch || :justin
+      str = Stream::Twitch.new(stream_uri, {update: false})
+    when :hashd
+      str = Stream::Hashd.new(stream_uri, {update: false})
+    end
 
     if object = cache.get(str.cache_id)
       return object
@@ -18,6 +24,8 @@ helpers do
       "http://www.twitch.tv/#{stream_id}"
     when :justin
       "http://www.justin.tv/#{stream_id}"
+    when :hashd
+      "http://hashd.tv/#{stream_id}"
     end
   end
 
