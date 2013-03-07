834a56ca98b83da14c060805846ccb836ba8e5c1
Johan Svensson <johan@atomicplayboy.net>
2013-03-07 12:08:59 +0100

    Use Stream::Cache
    Oops.

--- lib/stream_utils.rb
+++ lib/stream_utils.rb
@@ -29,7 +29,7 @@ class Stream
           result = {
             service: service,
             stream_id: stream_id,
-            cache_id: Digest::MD5.hexdigest("#{service}-#{stream_id}"),
+            cache_id: Stream::Cache.name(service, stream_id),
             json_uri: "#{StreamJsonUri[service]}#{stream_id}"
           }
           return result
